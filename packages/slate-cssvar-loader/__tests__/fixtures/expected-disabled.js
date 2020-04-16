a942034b38534b75b8cb7a8260aef99bb329111b
gil93 <gvelezjr02@gmail.com>
2020-04-16 01:04:44 -0400

    update cssvar tests due to changes in testing dependencies

--- packages/slate-cssvar-loader/__tests__/fixtures/expected-disabled.js
+++ packages/slate-cssvar-loader/__tests__/fixtures/expected-disabled.js
@@ -1,8 +1,7 @@
-exports = module.exports = require("../../node_modules/css-loader/lib/css-base.js")(false);
-// imports
-
-
-// module
+// Imports
+var ___CSS_LOADER_API_IMPORT___ = require("../../../../node_modules/css-loader/dist/runtime/api.js");
+exports = ___CSS_LOADER_API_IMPORT___(false);
+// Module
 exports.push([module.id, ".heading {\n  color: var(--headings_color);\n  background-color: '#FF00FF';\n  letter-spacing: var(--letter_spacing);\n}\n\n.title {\n  color: var(--headings_color);\n  font-weight: var(--font-body-bold-weight);\n}\n\nbody {\n  background-color: var(--body_color);\n}", ""]);
-
-// exports
+// Exports
+module.exports = exports;
--- packages/slate-cssvar-loader/__tests__/fixtures/expected.js
+++ packages/slate-cssvar-loader/__tests__/fixtures/expected.js
@@ -1,8 +1,7 @@
-exports = module.exports = require("../../node_modules/css-loader/lib/css-base.js")(false);
-// imports
-
-
-// module
+// Imports
+var ___CSS_LOADER_API_IMPORT___ = require("../../../../node_modules/css-loader/dist/runtime/api.js");
+exports = ___CSS_LOADER_API_IMPORT___(false);
+// Module
 exports.push([module.id, ".heading {\n  color: {{ settings.headings_color }};\n  background-color: '#FF00FF';\n  letter-spacing: {{ settings.letter_spacing }}px;\n}\n\n.title {\n  color: {{ settings.headings_color }};\n  font-weight: {{ font_body_bold.weight | default: \"bold\" }};\n}\n\nbody {\n  background-color: {{ settings.body_color }};\n}", ""]);
-
-// exports
+// Exports
+module.exports = exports;
